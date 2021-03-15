c7c15a28bc067fe31291deb6fa38bacfffb74a41
Jennifer Martelle <jennifermartelle@Jennifers-MBP.home>
2021-03-15 16:38:47 -0400

    heroku

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const mongoose = require("mongoose");
 
 //always needs a url, and / connects it to that db
 //this connects to our database
-mongoose.connect("mongodb://localhost:27017/til", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser:true,
+  useUnifiedTopology:true
 });
 
 //connects it to the til database
